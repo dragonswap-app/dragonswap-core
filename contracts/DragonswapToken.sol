6f044963811a0f0162fd6a1d363be08f652d4565
markolazic01 <markolazic01@gmail.com>
2024-02-07 12:38:58 +0100

    chore: Make token burnable

--- contracts/DragonswapToken.sol
+++ contracts/DragonswapToken.sol
@@ -2,9 +2,10 @@
 pragma solidity ^0.8.20;
 
 import "@openzeppelin/contracts/token/ERC20/ERC20.sol";
+import "@openzeppelin/contracts/token/ERC20/extensions/ERC20Burnable.sol";
 import "@openzeppelin/contracts/token/ERC20/extensions/ERC20Permit.sol";
 
-contract MyToken is ERC20, ERC20Permit {
+contract MyToken is ERC20, ERC20Burnable, ERC20Permit {
     constructor(address treasury) ERC20("Dragonswap Token", "DST") ERC20Permit("Dragonswap") {
         require(treasury != address(0));
         _mint(treasury, 1_000_000 * 1e18);
